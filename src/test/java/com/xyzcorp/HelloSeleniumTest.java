a64ab31b3c1f2aec989eb3c30f7420a764f86bae
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-04 16:21:41 -0500

    Formatted code

--- src/test/java/com/xyzcorp/HelloSeleniumTest.java
+++ src/test/java/com/xyzcorp/HelloSeleniumTest.java
@@ -13,19 +13,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSeleniumTest {
-    
+
     // Using a chrome webdriver for this example
     private static ChromeDriver chromeDriver;
 
     @BeforeAll
     public static void setup() {
-        // Setting up property manually. A true pain in the you know what. An absolute path too at that
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ernest.nguyen\\drivers\\chrome\\98\\chromedriver_win32\\chromedriver.exe");
+        // Setting up property manually. A true pain in the you know what. An absolute
+        // path too at that
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\ernest.nguyen\\drivers\\chrome\\98\\chromedriver_win32\\chromedriver.exe");
 
-        //Instantiate a new driver
+        // Instantiate a new driver
         chromeDriver = new ChromeDriver();
     }
-
 
     @Test
     public void testSearchAndGoToBddWiki() {
@@ -41,15 +42,15 @@ public class HelloSeleniumTest {
         chromeDriver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 
         // Click the first link: it should be a wiki link
-        WebElement wikiLink = chromeDriver.findElement(By.cssSelector("#rso > div:nth-child(1) > div.g.tF2Cxc > div > div.NJo7tc.Z26q7c.jGGQ5e > div > a > h3"));
+        WebElement wikiLink = chromeDriver.findElement(By
+                .cssSelector("#rso > div:nth-child(1) > div.g.tF2Cxc > div > div.NJo7tc.Z26q7c.jGGQ5e > div > a > h3"));
         assertTrue(wikiLink.getText().contains("Wikipedia"));
         wikiLink.click();
 
         // Assert that the title of the wiki page is "Behavior-driven development"
         String wikiPageTitle = chromeDriver.findElement(By.id("firstHeading")).getText();
         assertTrue(wikiPageTitle.equals(searchTerm));
-        
-        
+
     }
 
     @AfterAll
